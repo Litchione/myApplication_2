3a555452c5c5001c363c9b50eb38f441e5158a7d
kimho <rlagh1073@naver.com>
2022-02-04 00:54:42 +0900

    2022-0203-로그인성공시->게시글 목록(통신성공)_테스트용

--- app/src/main/java/com/example/myapplication_1/postlistActivity.java
+++ app/src/main/java/com/example/myapplication_1/postlistActivity.java
@@ -3,7 +3,7 @@ package com.example.myapplication_1;/*package com.example.myapplication_1;
 public class postlistActivity {
 
 }*/
-
+//123123123
 
         import androidx.appcompat.app.AppCompatActivity;
 
